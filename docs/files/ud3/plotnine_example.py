a92b8390f6993ecf5b45b12f1200a837465ff3ee
Joan Puigcerver <joapuiib@gmail.com>
2024-10-14 15:51:34 +0200

    plotnine

--- docs/files/ud3/plotnine_example.py
+++ docs/files/ud3/plotnine_example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from plotnine import ggplot, geom_point, aes
+from plotnine import *
 from plotnine.data import mtcars
 
 print("Dades `mtcars`:")
@@ -10,6 +10,28 @@ plot = (
     ggplot(mtcars)
     + aes("wt", "mpg", color="factor(gear)")
     + geom_point()
+    + labs(title="Consum (wt) vs Pes (mpg)",
+           x="Pes en lliures",
+           y="Consum en milles per galó",
+           color="Nombre de marxes"
+    )
+    + theme_dark()
 )
 
 plot.show()
+
+line_plot = (
+    ggplot(mtcars)
+    + aes(x='wt', y='mpg', color='factor(gear)')
+    + geom_line()
+)
+
+line_plot.show()
+
+bar_plot = (
+    ggplot(mtcars)
+    + aes(x='factor(cyl)', fill='factor(gear)')
+    + geom_bar()
+)
+
+bar_plot.show()
